070c9ea23d0bed7dac45bb882c3d7168e98dd32d
Hassan Mehmood <hassanmehmood.dev@gmail.com>
2025-03-15 17:23:48 +0500

    Fixed the dark theme issue

--- src/features/category/AllProductsByCategory.jsx
+++ src/features/category/AllProductsByCategory.jsx
@@ -38,51 +38,59 @@ export default function AllProductsByCategory() {
   };
 
   return (
-    <div className="px-6 py-8 container mx-auto min-h-screen">
-      <h2 className="text-2xl font-semibold text-green-700 mb-6">Products</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
-        {error && <p className="text-red-500">Something went wrong</p>}
-        {isLoading && <p>Loading...</p>}
-        {data?.products.map((product) => (
-          <Link
-            key={product._id}
-            to={`/available-stocks/${product._id}`}
-            className="bg-white border border-gray-200 rounded-lg shadow-lg p-4 flex flex-col items-center"
-          >
-            <h3 className="text-xl font-medium text-gray-800">
-              {product.name}
-            </h3>
-            <p className="text-sm text-gray-500 mt-2">CAS: {product.cas_no}</p>
-          </Link>
-        ))}
-      </div>
-
-      {data?.products.length === 0 && (
-        <p className="text-red-500 text-center">No products found</p>
-      )}
-
-      {/* Pagination Controls */}
-      {data && data.totalPages > 1 && (
-        <div className="flex justify-center items-center mt-10 gap-4">
-          <button
-            onClick={() => handlePageChange(page - 1)}
-            disabled={page === 1}
-            className="px-4 py-2 bg-[#8AA823] text-white rounded disabled:opacity-50"
-          >
-            Previous
-          </button>
-          <span>
-            Page {page} of {data.totalPages}
-          </span>
-          <button
-            onClick={() => handlePageChange(page + 1)}
-            disabled={page === data.totalPages}
-            className="px-4 py-2 bg-[#8AA823] text-white rounded disabled:opacity-50"
-          >
-            Next
-          </button>
+    <div className="px-6 py-8 min-h-screen dark:bg-background-dark">
+      <div className="container mx-auto ">
+        <h2 className="text-2xl font-semibold text-green-700 dark:text-white mb-6">
+          Products
+        </h2>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+          {error && <p className="text-red-500">Something went wrong</p>}
+          {isLoading && <p>Loading...</p>}
+          {data?.products.map((product) => (
+            <Link
+              key={product._id}
+              to={`/available-stocks/${product._id}`}
+              className="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-lg p-4 flex flex-col items-center"
+            >
+              <h3 className="text-xl font-medium text-gray-800 dark:text-white">
+                {product.name}
+              </h3>
+              <p className="text-sm text-gray-500 dark:text-gray-300 mt-2">
+                CAS: {product.cas_no}
+              </p>
+            </Link>
+          ))}
         </div>
-      )}
+
+        {data?.products.length === 0 && (
+          <p className="text-red-500 text-center dark:text-white">
+            No products found
+          </p>
+        )}
+
+        {/* Pagination Controls */}
+        {data && data.totalPages > 1 && (
+          <div className="flex justify-center items-center mt-10 gap-4">
+            <button
+              onClick={() => handlePageChange(page - 1)}
+              disabled={page === 1}
+              className="px-4 py-2 bg-[#8AA823] text-white rounded disabled:opacity-50"
+            >
+              Previous
+            </button>
+            <span className="text-gray-800 dark:text-white">
+              Page {page} of {data.totalPages}
+            </span>
+            <button
+              onClick={() => handlePageChange(page + 1)}
+              disabled={page === data.totalPages}
+              className="px-4 py-2 bg-[#8AA823] text-white rounded disabled:opacity-50"
+            >
+              Next
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
